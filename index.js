e2cf44a422582d79f04c9747c3170202daa87a7a
Mwenje <mwenje24@gmail.com>
2022-06-04 01:04:23 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function appendCat(name){
+    return [...cats, name];
+}
+
+function prependCat(name){
+    return [name, ...cats];
+}
+
+function removeFirstCat(){
+    return cats.slice(1)
+}
+
+function removeLastCat(){
+    return cats.slice(0, cats.length -1)
+}
